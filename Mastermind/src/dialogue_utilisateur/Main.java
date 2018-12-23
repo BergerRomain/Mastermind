f4f54b6ec2ad82e9239ab18feb4ade025bf94356
BergerRomain <xvongola98@gmail.com>
2018-12-23 16:32:21 +0100

    Ajout des classes joueur et solution

---
+++ Mastermind/src/dialogue_utilisateur/Main.java
@@ -0,0 +1,11 @@
+package dialogue_utilisateur;
+
+import metier.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Joueur s = new Joueur();
+	}
+}
---
+++ Mastermind/src/metier/Indices.java
@@ -0,0 +1,10 @@
+package metier;
+
+public class Indices extends Joueur
+{	
+	public Indices()
+	{
+		System.out.println("Il vous reste "+(count-1)+" essaies");
+		count--;
+	}
+}
